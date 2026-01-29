b824425b5dea7b10bb2dad72df1f37c28f4dfd32
Davy <100140271+DavyR01@users.noreply.github.com>
2026-01-29 10:32:26 +0100

    fix(test): restore next/jest resolution in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-import nextJest from "next/jest";
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
    dir: "./",
@@ -18,7 +18,7 @@ const customJestConfig = {
    },
 };
 
-export default createJestConfig(customJestConfig);
+module.exports = createJestConfig(customJestConfig);
 
 
 
